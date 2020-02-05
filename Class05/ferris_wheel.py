a629d445de5bcba5caf8814be7ae7c0c4aa588b1
Tom Helmuth <thelmuth@hamilton.edu>
2020-02-05 10:39:41 -0500

    Code from 2/5/19

--- Class05/ferris_wheel.py
+++ Class05/ferris_wheel.py
@@ -6,15 +6,18 @@ def main():
     height = float(input("Enter the height of the Ferris wheel in feet: "))
     distance = float(input("Enter the distance the ferris wheel has traveled in miles: "))
 
+    # Converting distance to feet and height to circumference
     distance_in_ft = distance * FEET_PER_MILE
     circumference = height * math.pi
 
-    rotations = distance_in_ft // circumference
-    feet_past_last_rotation = distance_in_ft % circumference
+    # This calculates the rotations of the Ferris wheel and the
+    # distance past the last rotation
+    rotations = int(distance_in_ft // circumference)
+    feet_past_last_rotation = round(distance_in_ft % circumference)
 
-    print("The ferris wheen rotated", rotations, "times.")
+    print("The ferris wheen rotated", rotations, "times.") # This prints rotations
     print("The outside traveled", feet_past_last_rotation, "feet past the last rotation.")
-    
+
 
 
 
---
+++ Class07/warmup.py
@@ -0,0 +1,7 @@
+
+# What does this print:
+
+test = "This is a test."
+a = test.find("is")
+b = test.find(".")
+print(test[a:b])
